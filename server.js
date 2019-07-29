20e0bb2a684b92fb359785bd6bf22aaf66d1c38c
juanan-hernandez <juanantoniohb93@gmail.com>
2019-07-29 22:52:01 +0200

    updated package angular-devkit just for development

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8000);
